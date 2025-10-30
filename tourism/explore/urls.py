e040f38f36fe1f021442e7770f2339e1fc9afa18
Theko Joseph <thekothekolerato@gmail.com>
2025-10-30 15:39:43 +0200

    Enhancement

--- tourism/explore/urls.py
+++ tourism/explore/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name = "welcome"),
+    path('features', views.feature, name='features'),
+    path('about', views.about, name='about'),
 ]
--- tourism/explore/views.py
+++ tourism/explore/views.py
@@ -5,4 +5,15 @@ def base(request):
     return render(request, 'explore/landing/base.html', {})
 
 
+def feature(request):
+    return render(request, 'explore/landing/features.html', {})
 
+
+
+def about(request):
+    return render(request, 'explore/landing/about.html', {})
+
+
+
+
+    
